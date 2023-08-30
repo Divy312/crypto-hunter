73b4871fa3fbadb56419df8c6a5931bb2fc9947e
Divy31245 <zaladivyb@gmail.com>
2023-08-30 20:57:14 +0530

    completed the project

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA6z7RRNMJI8Vg1MxEjBlh16CCBDku4TXQ",
+  authDomain: "demo1-87c5d.firebaseapp.com",
+  projectId: "demo1-87c5d",
+  storageBucket: "demo1-87c5d.appspot.com",
+  messagingSenderId: "1089172897760",
+  appId: "1:1089172897760:web:51cb59edaa9ce9afacf585",
+  measurementId: "G-J8ZZMDRYQG"
+};
+
+export default firebaseConfig;
